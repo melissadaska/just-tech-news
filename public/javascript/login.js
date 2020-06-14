2eec374632310807d2d1c61e230e085ac284952c
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-14 09:16:34 -0500

    create login, front end logic, session on backend and logic to destroy session

---
+++ public/javascript/login.js
@@ -0,0 +1,55 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+        // by using await, we can assign the result of the promise to a variable that way we don't need to use catch() or then() to tell the code what to do after the Promise completes
+      const response = await fetch('/api/users', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      // add error handling to check response status
+      if (resposnse.ok) {
+        console.log('success');
+      } else {
+          alert(response.statusText);
+      }
+    }
+}
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+  
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
